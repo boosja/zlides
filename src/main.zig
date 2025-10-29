ed30cba46b984b0df3f56bcffb871797d7b0ba8a
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-10-29 11:48:47 +0100

    Parse slides and start process loop

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Process = @import("process.zig").Process;
+const splitPages = @import("slides.zig").splitPages;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -10,8 +12,19 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
     const filepath = args[1];
 
-    const slides = try std.fs.cwd().readFileAlloc(allocator, filepath, 1024);
-    defer allocator.free(slides);
+    const content = try std.fs.cwd().readFileAlloc(allocator, filepath, 1024);
+    //defer allocator.free(content);
 
-    std.debug.print("{s}\n", .{slides});
+    _ = try splitPages(allocator, content, "---");
+    //defer allocator.free(slides);
+
+    // process handling
+    var process = try Process.init();
+    defer process.deinit();
+
+    var buf: [1]u8 = undefined;
+    while (buf[0] != 'q') {
+        _ = try process.read(&buf);
+        _ = try process.write(&buf);
+    }
 }
