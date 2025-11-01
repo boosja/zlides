7755700ca6ea34a8b2338a9712ce51c1d3a6f444
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-11-01 10:40:03 +0100

    Add some comments

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,18 @@ const std = @import("std");
 const Zlides = @import("zlides").Zlides;
 
 pub fn main() !void {
+    // Initialize ArenaAllocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // Free the allocated memory when out of scope
     defer arena.deinit();
 
     const allocator = arena.allocator();
 
+    // Read command line args
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
-    const filepath = args[1];
 
+    const filepath = args[1];
     const content = try std.fs.cwd().readFileAlloc(allocator, filepath, 1024);
     //defer allocator.free(content);
 
