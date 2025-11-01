137ce2d9be8728350923741cf3ccb06a35e6a874
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-11-01 10:35:55 +0100

    Move slideshow logic to root module

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
-const Process = @import("process.zig").Process;
-const makeSlides = @import("slides.zig").makeSlides;
+const Zlides = @import("zlides").Zlides;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -15,58 +14,6 @@ pub fn main() !void {
     const content = try std.fs.cwd().readFileAlloc(allocator, filepath, 1024);
     //defer allocator.free(content);
 
-    const slides = try makeSlides(allocator, content);
-
-    // process handling
-    var process = try Process.init();
-    defer process.deinit();
-
-    var page: usize = 0;
-    var cmd_buf: [1]u8 = .{'^'}; // or "^".*
-    while (true) {
-        const cmd = cmd_buf[0];
-
-        const newPage: usize, const status: Status =
-            switch (cmd) {
-                '^' => .{ page, .OK },
-                ' ' => forward(1, page, slides.len),
-                'j' => forward(1, page, slides.len),
-                'J' => forward(5, page, slides.len),
-
-                'k' => backward(1, page),
-                'K' => backward(5, page),
-
-                'q' => break,
-                else => .{ page, .NOOP },
-            };
-
-        page = newPage;
-
-        _ = try process.clear();
-        _ = try process.write(slides[page]);
-        _ = try process.write("\n");
-
-        var buf: [7]u8 = undefined;
-        const pagination = try std.fmt.bufPrint(&buf, "[{}/{}]", .{ page + 1, slides.len });
-        _ = try process.write(pagination);
-
-        const info = switch (status) {
-            .FIRST_SLIDE => " beginning",
-            .LAST_SLIDE => " end",
-            else => "",
-        };
-        _ = try process.write(info);
-
-        _ = try process.read(&cmd_buf);
-    }
-}
-
-const Status = enum { OK, FIRST_SLIDE, LAST_SLIDE, NOOP };
-
-fn forward(step: usize, page: usize, max: usize) struct { usize, Status } {
-    return if (page + step < max) .{ page + 1, .OK } else .{ max - 1, .LAST_SLIDE };
-}
-
-fn backward(step: usize, page: usize) struct { usize, Status } {
-    return if (0 < page -| step) .{ page - step, .OK } else .{ 0, .FIRST_SLIDE };
+    var zlides = try Zlides.makeFrom(allocator, content);
+    try zlides.show();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,76 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const makeSlides = @import("slides.zig").makeSlides;
+const Process = @import("process.zig").Process;
 
+pub const Zlides = struct {
+    slides: []const []const u8,
+
+    pub fn makeFrom(allocator: Allocator, raw: []u8) !Zlides {
+        return .{
+            .slides = try makeSlides(allocator, raw),
+        };
+    }
+
+    pub fn show(self: *Zlides) !void {
+        const slides = self.slides;
+
+        var process = try Process.init();
+        defer process.deinit();
+
+        var page: usize = 0;
+        var cmd_buf: [1]u8 = .{'^'}; // or "^".*
+        while (true) {
+            const cmd = cmd_buf[0];
+
+            const newPage: usize, const status: Status =
+                switch (cmd) {
+                    '^' => .{ page, .OK },
+                    ' ' => forward(1, page, slides.len),
+                    'j' => forward(1, page, slides.len),
+                    'J' => forward(5, page, slides.len),
+
+                    'k' => backward(1, page),
+                    'K' => backward(5, page),
+
+                    'q' => break,
+                    else => .{ page, .NOOP },
+                };
+
+            page = newPage;
+
+            _ = try process.clear();
+            _ = try process.write(slides[page]);
+            _ = try process.write("\n");
+
+            var buf: [7]u8 = undefined;
+            const pagination = try std.fmt.bufPrint(&buf, "[{}/{}]", .{ page + 1, slides.len });
+            _ = try process.write(pagination);
+
+            const info = switch (status) {
+                .FIRST_SLIDE => " beginning",
+                .LAST_SLIDE => " end",
+                else => "",
+            };
+            _ = try process.write(info);
+
+            _ = try process.read(&cmd_buf);
+        }
+    }
+};
+
+const Status = enum { OK, FIRST_SLIDE, LAST_SLIDE, NOOP };
+
+fn forward(step: usize, page: usize, max: usize) struct { usize, Status } {
+    return if (page + step < max) .{ page + 1, .OK } else .{ max - 1, .LAST_SLIDE };
+}
+
+fn backward(step: usize, page: usize) struct { usize, Status } {
+    return if (0 < page -| step) .{ page - step, .OK } else .{ 0, .FIRST_SLIDE };
+}
+
+// From zig init:
 pub fn bufferedPrint() !void {
     // Stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
@@ -12,12 +82,4 @@ pub fn bufferedPrint() !void {
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     try stdout.flush(); // Don't forget to flush!
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
 }
